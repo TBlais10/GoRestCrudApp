31f1a1d36b425a81c6bcaf6815d26ce01f04ba3e
TBlais10 <blaislighting@gmail.com>
2021-11-17 22:41:58 -0500

    Initial commit

---
+++ src/main/java/careerdevs/springboot/GoRestCrudApp/GoRestCrudAppApplication.java
@@ -0,0 +1,13 @@
+package careerdevs.springboot.GoRestCrudApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoRestCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoRestCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/careerdevs/springboot/GoRestCrudApp/GoRestCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package careerdevs.springboot.GoRestCrudApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoRestCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
